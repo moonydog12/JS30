89990a408ddfb1133aa7e4b806139b72731cf195
lsd <lsd@ehk.com.tw>
2022-07-26 15:11:06 +0800

    finish 05

---
+++ 05 - Flex Panel Gallery/script.js
@@ -0,0 +1,18 @@
+const panels = document.querySelectorAll('.panel');
+panels.forEach((panel) => panel.addEventListener('click', toggleOpen));
+
+panels.forEach((panel) =>
+  panel.addEventListener('transitionend', toggleActive)
+);
+
+function toggleOpen() {
+  this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+  console.log(e.propertyName);
+  if (e.propertyName.includes('flex')) {
+    console.log(this);
+    this.classList.toggle('active');
+  }
+}
