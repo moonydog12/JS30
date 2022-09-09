0d60422785c5000a1bb07baf96b62046b62208e7
xolodog <losididog@gmail.com>
2022-09-09 22:27:41 +0800

    Add project 21

---
+++ 21 - Geolocation/script.js
@@ -0,0 +1,13 @@
+'use strict';
+const arrow = document.querySelector('.arrow');
+const speed = document.querySelector('.speed-value');
+
+navigator.geolocation.watchPosition(
+  (data) => {
+    speed.textContent = Math.round(data.coords.speed);
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+  },
+  (err) => {
+    console.error(err);
+  }
+);
