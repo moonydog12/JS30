37932d738f4d39db7b7850c1b454fe044f1d7327
lsd <lsd@ehk.com.tw>
2022-10-26 09:02:52 +0800

    Add comments

--- 34 - Exchange Rate Calculator Fetch & JSON Intro/script.js
+++ 34 - Exchange Rate Calculator Fetch & JSON Intro/script.js
@@ -5,8 +5,8 @@ const amountTwo = document.querySelector('#amount-two');
 
 const rateEl = document.querySelector('#rate');
 const swapBtn = document.querySelector('#swap');
-const countries = ['HKD', 'JPY', 'TWD', 'USD'];
-
+const countries = ['HKD', 'JPY', 'TWD', 'USD', 'EUR', 'GBP', 'CNY', 'KRW', 'CHF'];
+countries.sort((a, b) => (a[0] > b[0] ? 1 : -1));
 // Functions
 // Generate UI
 countries.forEach((country) => {
@@ -18,29 +18,39 @@ countries.forEach((country) => {
 
 countries.forEach((country) => {
   const optionEl = document.createElement('option');
+  if (country === 'TWD') {
+    optionEl.setAttribute('selected', true);
+  }
   optionEl.setAttribute('name', country);
   optionEl.innerText = country;
   currencyTwo.append(optionEl);
 });
 
-// Fetch exchange rates and update DOM
+// Fetch exchange rates and update DOM (Async/Await)
 const calculateRate = async () => {
   const currencyOneValue = currencyOne.value;
   const currencyTwoValue = currencyTwo.value;
-  // fetch(`https://open.exchangerate-api.com/v6/latest/${currencyOneValue}`)
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     // console.log(data);
-  //     const rate = data.rates[currencyTwoValue];
-  //     rateEl.textContent = `1 ${currencyOneValue} = ${rate} ${currencyTwoValue}`;
-  //     amountTwo.value = (amountOne.value * rate).toFixed(2);
-  //   });
+
   let currencyData = await fetch(`https://open.exchangerate-api.com/v6/latest/${currencyOneValue}`);
   currencyData = await currencyData.json();
   const rate = currencyData.rates[currencyTwoValue];
   rateEl.textContent = `1 ${currencyOneValue} = ${rate} ${currencyTwoValue}`;
   amountTwo.value = (amountOne.value * rate).toFixed(2);
 };
+
+// Fetch exchange rates and update DOM (Promise chain)
+
+// const calculateRateWithFetch = () => {
+//   const currencyOneValue = currencyOne.value;
+//   const currencyTwoValue = currencyTwo.value;
+//   fetch(`https://open.exchangerate-api.com/v6/latest/${currencyOneValue}`)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       const rate = data.rates[currencyTwoValue];
+//       rateEl.textContent = `1 ${currencyOneValue} = ${rate} ${currencyTwoValue}`;
+//       amountTwo.value = (amountOne.value * rate).toFixed(2);
+//     });
+// };
 
 // Event listeners
 currencyOne.addEventListener('change', calculateRate);
