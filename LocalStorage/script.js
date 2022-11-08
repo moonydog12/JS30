1792e3ea5f27c30da7d02d6a83ea1b7b1271adb8
lsd <lsd@ehk.com.tw>
2022-11-08 14:20:08 +0800

    tweak localstorage project

--- LocalStorage/script.js
+++ LocalStorage/script.js
@@ -1,48 +1,59 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
+const deleteButton = document.querySelector('#deleteAll');
 const items = JSON.parse(localStorage.getItem('items')) || [];
 
-const populateList = function renderHtml(platesList, plates = []) {
-  platesList.innerHTML = plates
-    .map((plate, i) => {
-      return `
-    <li>
-    <input type="checkbox" data-index="${i}" id="item${i}" ${plate.done ? 'checked' : null} />
-    <label for="item${i}">${plate.text}</label>
-  </li>
-    `;
-    })
-    .join('');
+const populateList = (plates) => {
+  if (!plates || plates.length < 1) {
+    itemsList.innerHTML = '<li class="loading-list">Loading Tapas...</li>';
+  } else {
+    const platesList = plates
+      .map(
+        (plate, i) => `
+        <li>
+          <input type="checkbox" data-index="${i}" id="item${i}" ${plate.done ? 'checked' : null} />
+          <label for="item${i}">${plate.text}</label>
+        </li>`,
+      )
+      .join('');
+    itemsList.innerHTML = platesList;
+  }
 };
 
-function addItem(e) {
-  // 關閉表單 submit 會重新導向的預設行為
+const addItem = (e) => {
   e.preventDefault();
-  const text = this.querySelector('input').value; // this 指向觸發e的元素，在這裡會指向 form
+  const form = e.target;
+  const text = form.querySelector('input').value;
   const item = {
-    // es6 shorthand
-    text, // text: text,
+    text,
     done: false,
   };
-
   items.push(item);
-  populateList(itemsList, items);
-  // 把資料存入 localStorage
-  /* localStorage 只能存 string，所以會把物件轉成字串，
-    所以先用JSON.stringify提前轉成字串 */
-  localStorage.setItem('items', JSON.stringify(items));
-  this.reset();
-}
+  populateList(items);
 
-function toggleDone(e) {
-  if (!e.target.matches('input')) return; // 跳過非 input 的元素
+  // LocalStorage only store string,so we use stringify to convert it beforehand
+  localStorage.setItem('items', JSON.stringify(items));
+  form.reset();
+};
+
+const deleteAllItems = () => {
+  for (let i = items.length; i > 0; i--) {
+    items.pop(items[i]);
+  }
+  localStorage.removeItem('items');
+  populateList(items);
+};
+
+const toggleDone = (e) => {
+  if (!e.target.matches('input')) return;
   const el = e.target;
   const { index } = el.dataset;
   items[index].done = !items[index].done;
   localStorage.setItem('items', JSON.stringify(items));
-  populateList(items, itemsList);
-}
+  populateList(items);
+};
 
 addItems.addEventListener('submit', addItem);
+deleteButton.addEventListener('click', deleteAllItems);
 itemsList.addEventListener('click', toggleDone);
-populateList(items, itemsList);
+populateList(items);
